873efab0a036422855285028a58a78851014def3
Ashley <ashley.white@infrotex.co.uk>
2018-05-11 12:59:36 +0000

    using if statment

--- modules/nginx_app/manifests/init.pp
+++ modules/nginx_app/manifests/init.pp
@@ -1,9 +1,21 @@
 #
 class nginx_app {
 
-  class{'nginx':}
+  include chocolatey
+
+  if $facts['kernel'] == 'windows' {
+    Package { 'nginx':
+    ensure => '1.12.2',
+    source => 'chocolatey'
+  }
+  } else {
+  package { 'nginx':
+    ensure => '1.12.2',
+  }
+
 
   # file { "index.html":
   #   ensure  => file,
   #   content => template("${module_name}/index.html.erb"),
+}
 }
