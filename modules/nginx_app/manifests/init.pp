4b57f22118500518a481a1a6eaca036ecfd0efc8
Ashley <ashley.white@infrotex.co.uk>
2018-05-11 13:35:38 +0000

    just linux

--- modules/nginx_app/manifests/init.pp
+++ modules/nginx_app/manifests/init.pp
@@ -1,22 +1,25 @@
 #
-class nginx_app {
+class nginx_app (
 
+  )
+{
   include chocolatey
 
-  if $facts['kernel'] == 'Linux' {
+  if $facts['kernel'] ~= /[Ll]inux/ {
     package { 'nginx':
-    ensure   => 'latest',
-
-  }
-  } else {
-  package { 'nginx':
-    ensure   => latest,
-    provider => 'chocolatey',
-  }
-
-
-  # file { "index.html":
-  #   ensure  => file,
-  #   content => template("${module_name}/index.html.erb"),
-}
-}
+     ensure   => latest,
+   }
+ }
+#   }
+#   else {
+#   package { 'nginx':
+#     ensure   => latest,
+#     provider => 'chocolatey',
+#   }
+#
+#
+#   # file { "index.html":
+#   #   ensure  => file,
+#   #   content => template("${module_name}/index.html.erb"),
+# }
+# }
