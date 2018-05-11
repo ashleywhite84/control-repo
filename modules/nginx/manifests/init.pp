c28a48594b9eeef6bceac39d031653a3c39d4920
Ashley <ashley.white@infrotex.co.uk>
2018-05-11 12:30:59 +0000

    nginx stuff and module

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,9 +1,16 @@
 #
-class nginx {
+class nginx (
+  string $path = ''
+  )
+
+{
 
     include nginx
   Package{'nginx':
   ensure => 'latest',
 # source => 'yum',
   }
+  file { "index.html":
+    ensure  => file,
+    content => template("${module_name}/index.html.erb"),
 }
